7b6f0c1afe95a7d8959539f5d12521a7b9d9073e
Michael Gustavsson <michael.gustavsson@softtech-dev.se>
2026-09-17 15:01:00 +0200

    End of lesson 8

---
+++ Lektion-08/atm/Account.cs
@@ -0,0 +1,43 @@
+﻿namespace atm;
+
+public class Account(string accountNo)
+{
+    private int _balance;
+
+    public string AccountNumber { get; } = accountNo;
+    public virtual int Balance
+    {
+        get { return _balance; }
+        private set { _balance = value; }
+    }
+    public IList<Transaction> Transactions { get; } = [];
+    public void Deposit(int amount)
+    {
+        Balance += amount;
+        AddTransaction(amount, TransactionTypeEnum.Insättning);
+    }
+    public void WithDraw(int amount)
+    {
+        if (Balance < amount)
+        {
+            throw new Exception("Du har inte tillräckligt på kontot!");
+        }
+        Balance -= amount;
+        AddTransaction(amount, TransactionTypeEnum.Uttag);
+    }
+
+    public override string ToString()
+    {
+        return $"AccountNumber: {AccountNumber} Balance: {Balance} AccountType: {this.GetType()}";
+    }
+
+    private void AddTransaction(int amount, TransactionTypeEnum transactionType)
+    {
+        var tran = new Transaction();
+        tran.transactionDate = DateTime.Now;
+        tran.transactionType = transactionType;
+        tran.transactionValue = amount;
+
+        Transactions.Add(tran);
+    }
+}
---
+++ Lektion-08/atm/SavingsAccount.cs
@@ -0,0 +1,17 @@
+﻿namespace atm;
+
+public class SavingsAccount(string accountNo)
+    : Account(accountNo)
+{
+    const double INTEREST_RATE = 0.05;
+
+    public override int Balance
+    {
+        get { return Convert.ToInt32(base.Balance * (1 + INTEREST_RATE)); }
+    }
+
+    public override string ToString()
+    {
+        return $"AccountNumber: {AccountNumber} Balance: {Balance} InterestRate: {INTEREST_RATE} AccountType: {this.GetType()}";
+    }
+}
